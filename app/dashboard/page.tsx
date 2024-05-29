17a9b1bca5d427b58b69456e61d9eed37b555d29
deltrexgg <deltrexgg@gmail.com>
2024-05-29 23:13:00 +0530

    dev text 	modified:   app/dashboard/page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -10,7 +10,7 @@ function Dashboard() {
   useEffect(() => {
     if (!sessionStorage.getItem('userid')) {
       window.location.href = '/login';
-      //redirect
+      //redirect in dev
     }
 
     const ws = new WebSocket('wss://assignment-server-zx9x.onrender.com');
